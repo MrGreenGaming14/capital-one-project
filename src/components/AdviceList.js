2b097f2df3d3fc78a3d780fc67a7c219cfbbd9c3
MrGreenGaming <37852707+MrGreenGaming14@users.noreply.github.com>
2023-08-03 17:02:58 -0500

    modified:   src/components/AdviceList.css 	modified:   src/components/AdviceList.js 	modified:   src/components/FinancialAdviceTemplate.css 	modified:   src/data/adviceData.js

--- src/components/AdviceList.js
+++ src/components/AdviceList.js
@@ -1,14 +1,15 @@
 import {data} from '../data/adviceData';
+import {useState} from 'react';
 import AdviceTemplate from './FinancialAdviceTemplate';
 import './AdviceList.css';
 
 const Advice = () => {
     return (
-        <div>
+        <div className="container">
             <h1 className="title">Click the tiles below to learn more about your finances.</h1>
-            <div className="advice">
+            <div className="adviceContainer">
                 {data.map((advice, index) => (
-                    <AdviceTemplate name={advice.name} type={advice.type} image={advice.img} key={index}/>
+                    <AdviceTemplate name={advice.name} image={advice.image} key={index}/>
                 ))}
             </div>
         </div>
--- src/data/adviceData.js
+++ src/data/adviceData.js
@@ -1,21 +1,21 @@
 export const data = [
     {
-        img: "https://th.bing.com/th/id/OIP.8Twj50mWoMcy5z36tzlMKwAAAA?pid=ImgDet&rs=1",
+        image: "https://th.bing.com/th/id/OIP.8Twj50mWoMcy5z36tzlMKwAAAA?pid=ImgDet&rs=1",
         name: "IRA",
         type: "Retirement"
     },
     {
-        img: "https://shesavesshetravels.com/wp-content/uploads/2020/01/50-30-20-budget-rule-pinterest.jpg",
+        image: "https://thefinancialdiet.com/wp-content/uploads/2021/05/50-30-20-1.jpg",
         name: "50-30-20 Rule",
         type: "Budgeting"
     },
     {
-        img: "https://www.familyvest.com/wp-content/uploads/2019/01/emergency-savings2.jpg",
+        image: "https://www.familyvest.com/wp-content/uploads/2019/01/emergency-savings2.jpg",
         name: "Emergency Fund",
         type: "Saving"
     },
     {
-        img: "https://i.pinimg.com/originals/d9/cf/6d/d9cf6d1e25e5ec1b06e1f94286c1a075.png",
+        image: "https://i.pinimg.com/originals/d9/cf/6d/d9cf6d1e25e5ec1b06e1f94286c1a075.png",
         name: "Investing In Your 401k",
         type: "Retirement"
     }
