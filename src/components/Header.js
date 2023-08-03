894da5bb5cc00c7d53f4e8e88b5d71ba15d2dfd8
Stanley <stko2204@gmail.com>
2023-08-03 12:35:35 -0400

    modified:   package-lock.json modified:   package.json modified:   src/App.js new file:   src/components/Capital-One-Emblem-Blue.jpg new file:   src/components/Capital-One-Logo-White.png modified:   src/components/Header.css modified:   src/components/Header.js new file:   src/components/capital-one-project - Shortcut.lnk new file:   src/components/user_profile.avif

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,42 @@
 import './Header.css';
+import myLogo from './Capital-One-Emblem-Blue.jpg';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className="header">
-            <h1 className="title">My Personal Page</h1>
+            <img src={myLogo} width={177.78} height={100} />
+            Welcome, Stanley.
             <div className="links">
-                <a href="/">About Me</a>
-                <a href="/projects">Projects</a>
-                <a href="/resume">Resume</a>
-                <a href="/blog">Blog</a>
-                <a href="/social-media">Socials</a>
+                <a href="/">Home</a>
+                <a href="/1">1</a>
+                <a href="/2">2</a>
+                <a href="/3">3</a>
+                <a href="/4">4</a>
+                <a href="/5">5</a>
+                <a href="/6">6</a>
             </div>
         </div>
     );
 }
 
 export default Header;
+
+// import './Header.css';
+
+// const Header = () => {
+//     return (
+//         <div className="header">
+//             <h1 className="title">My Personal Page</h1>
+//             <div className="links">
+//                 <a href="/">About Me</a>
+//                 <a href="/projects">Projects</a>
+//                 <a href="/resume">Resume</a>
+//                 <a href="/blog">Blog</a>
+//                 <a href="/social-media">Socials</a>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Header;
