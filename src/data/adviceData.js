e64a835de4790e415cfa34344697ab38ad011638
MrGreenGaming <37852707+MrGreenGaming14@users.noreply.github.com>
2023-08-03 14:16:02 -0500

    new file:   src/components/AdviceList.css 	new file:   src/components/AdviceList.js 	new file:   src/components/FinancialAdviceTemplate.css 	new file:   src/components/FinancialAdviceTemplate.js 	new file:   src/data/adviceData.js

---
+++ src/data/adviceData.js
@@ -0,0 +1,23 @@
+export const data = [
+    {
+        img: "https://th.bing.com/th/id/OIP.rDR-U08byupLSuVVR_2r3QHaMa?pid=ImgDet&rs=1",
+        name: "Roth IRA",
+        type: "Retirement"
+    },
+    {
+        img: "https://shesavesshetravels.com/wp-content/uploads/2020/01/50-30-20-budget-rule-pinterest.jpg",
+        name: "50-30-20 Rule",
+        type: "Budgeting"
+    },
+    {
+        img: "https://www.familyvest.com/wp-content/uploads/2019/01/emergency-savings2.jpg",
+        name: "Emergency Fund",
+        type: "Saving"
+    },
+    {
+        img: "https://i.pinimg.com/originals/d9/cf/6d/d9cf6d1e25e5ec1b06e1f94286c1a075.png",
+        name: "Investing In Your 401k",
+        type: "Retirement"
+    }
+
+];
