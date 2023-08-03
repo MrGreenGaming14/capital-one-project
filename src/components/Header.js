3d940650ada99c5e802661ed2ec65319be008dfd
MrGreenGaming <37852707+MrGreenGaming14@users.noreply.github.com>
2023-08-03 11:33:51 -0500

    modified:   src/components/Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,11 @@ const Header = () => {
         <div className="header">
             <h1 className="title">My Personal Page</h1>
             <div className="links">
-                <a href="/">About Me</a>
+                <a href="/">About Mes</a>
                 <a href="/projects">Projects</a>
-                <a href="/resume">Resume</a>
-                <a href="/blog">Blog</a>
-                <a href="/social-media">Social</a>
+                <a href="/resume">Resumes</a>
+                <a href="/blog">Blogs</a>
+                <a href="/social-media">Socials</a>
             </div>
         </div>
     );
