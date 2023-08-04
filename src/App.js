bea4b01ceaccd1ff206b1227966dc3a56ddc1606
Stanley <stko2204@gmail.com>
2023-08-04 00:24:10 -0400

    modified:   src/App.js 	modified:   src/components/AdviceList.js 	new file:   src/components/Budgeting.css 	new file:   src/components/Budgeting.js 	modified:   src/components/Header.css 	modified:   src/components/Header.js 	new file:   src/components/IRA.css 	new file:   src/components/IRA.js 	new file:   src/components/Retirement.css 	new file:   src/components/Saving.css 	new file:   src/components/Saving.js 	modified:   src/data/adviceData.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Advice from './components/AdviceList';
 import Accounts from './components/AccountHome';
 import Transactions from './components/TransactionHome';
+import Saving from './components/Saving';
+import IRA from './components/IRA';
+import Budgeting from './components/Budgeting';
+import Retirement from './components/Retirement';
 
 function App() {
   return (
@@ -14,6 +18,10 @@ function App() {
           <Route path="/" element={<Accounts/>}/>
           <Route path="/transactions" element={<Transactions/>}/>
           <Route path="/advice" element={<Advice/>}/>
+          <Route path="/advice/saving" element={<Saving/>}/>
+          <Route path="/advice/ira" element={<IRA/>}/>
+          <Route path="/advice/budgeting" element={<Budgeting/>}/>
+          <Route path="/advice/retirement" element={<Retirement/>}/>
           <Route path="/goals" element={<h1>Goals</h1>}/>
         </Routes>
       </Router>
--- src/components/AdviceList.js
+++ src/components/AdviceList.js
@@ -1,19 +1,53 @@
-import {data} from '../data/adviceData';
-import {useState} from 'react';
+import React from 'react';
+import { data } from '../data/adviceData';
+import { Link } from 'react-router-dom';
 import AdviceTemplate from './FinancialAdviceTemplate';
 import './AdviceList.css';
 
 const Advice = () => {
-    return (
-        <div className="container">
-            <h1 className="title">Click the tiles below to learn more about your finances.</h1>
-            <div className="adviceContainer">
-                {data.map((advice, index) => (
-                    <AdviceTemplate name={advice.name} image={advice.image} key={index}/>
-                ))}
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="container">
+      <h1 className="title">Click the tiles below to learn more about your finances.</h1>
+      <div className="adviceContainer">
+        {data.map((advice, index) => (
+          <Link to={advice.web} key={index}>
+            <AdviceTemplate name={advice.name} image={advice.image} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Advice;
+
+// import {data} from '../data/adviceData';
+// import {useState} from 'react';
+// import AdviceTemplate from './FinancialAdviceTemplate';
+// import './AdviceList.css';
+// import {Link} from 'react-router-dom';
+
+// const Advice = () => {
+//     return (
+//         <div className="container">
+//             <h1 className="title">Click the tiles below to learn more about your finances.</h1>
+//             <div className="adviceContainer">
+//                 {data.map((advice, index) => (
+//                     <AdviceTemplate name={advice.name} image={advice.image} key={index}/>
+//                 ))}
+
+
+
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Advice;
+
+{/* <div className="links">
+<Link to= web> </Link>
+<Link to="/transactions">Transactions</Link>
+<Link to="/advice">Financial Advice</Link>
+<Link to="/goals">Goals</Link>
+</div> */}
